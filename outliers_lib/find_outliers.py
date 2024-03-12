fa3af88700321ee5421f0fea7ace1ca2de4f8e84
NovikovaEA <novikovaea@bk.ru>
2024-03-12 11:07:05 +0300

    second commit

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -1,4 +1,19 @@
+'''
+hjh
+jjjjjjjjjh'''
+
 def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+        feature (_type_): _description_
+        left (float, optional): _description_. Defaults to 0.01.
+        right (float, optional): _description_. Defaults to 0.99.
+
+    Returns:
+        _type_: _description_
+    """
     #changes
     x = data[feature]
     lower_bound = x.quantile(left)
@@ -6,3 +21,7 @@ def find_outliers_quantile(data, feature, left=0.01, right=0.99):
     outliers = data[(x < lower_bound) | (x > upper_bound)]
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
+
+
+def new_function(a,b):
+    pass 
