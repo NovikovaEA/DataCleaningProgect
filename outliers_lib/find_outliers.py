f081526f0bd920da59acccbb425c9de3955083a1
NovikovaEA <novikovaea@bk.ru>
2024-03-13 13:06:29 +0300

    Revert "second commit"
    This reverts commit 062375f66b4bbda3b197cc7e0c3ed62d23959375.

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -1,19 +1,4 @@
-'''
-hjh
-jjjjjjjjjh'''
-
 def find_outliers_quantile(data, feature, left=0.01, right=0.99):
-    """_summary_
-
-    Args:
-        data (_type_): _description_
-        feature (_type_): _description_
-        left (float, optional): _description_. Defaults to 0.01.
-        right (float, optional): _description_. Defaults to 0.99.
-
-    Returns:
-        _type_: _description_
-    """
     #changes
     x = data[feature]
     lower_bound = x.quantile(left)
@@ -21,7 +6,3 @@ def find_outliers_quantile(data, feature, left=0.01, right=0.99):
     outliers = data[(x < lower_bound) | (x > upper_bound)]
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
-
-
-def new_function(a,b):
-    pass 
