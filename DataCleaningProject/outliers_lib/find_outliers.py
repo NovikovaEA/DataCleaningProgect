a9c86c304afe99bc8128369f664b6758505ce0c0
NovikovaEA <novikovaea@bk.ru>
2024-03-26 18:17:33 +0300

    q

--- DataCleaningProject/outliers_lib/find_outliers.py
+++ DataCleaningProject/outliers_lib/find_outliers.py
@@ -1,8 +1,9 @@
 def find_outliers_quantile(data, feature, left=0.01, right=0.99):
-    #changes
+    #update
     x = data[feature]
     lower_bound = x.quantile(left)
     upper_bound = x.quantile(right)
     outliers = data[(x < lower_bound) | (x > upper_bound)]
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
+
